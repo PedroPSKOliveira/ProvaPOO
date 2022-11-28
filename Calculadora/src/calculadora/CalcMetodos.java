295d7580d221c4a4bb3441de8a5340a413a3a3a5
PjeterPSK1 <ped1567ro@gmail.com>
2022-11-28 17:40:43 -0300

    Prova.

---
+++ Calculadora/src/calculadora/CalcMetodos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Bruno
+ */
+public interface CalcMetodos {
+    
+    String somar(double v1, double v2);
+
+    String subtrair(double v1, double v2);
+
+    String mult(double v1, double v2);
+
+    String pot(double v1, double v2);
+
+    String rad(double v1);
+    
+    String div(double v1, double v2);
+}
